74a320fbbdbba313136c1a23344da58f7baca386
AdrianFebre <adrifeb@mac.com>
2022-08-31 21:46:15 -0700

    added pandas-driven p&l

--- MenuDjinn/inventory/views.py
+++ MenuDjinn/inventory/views.py
@@ -9,6 +9,7 @@ from .forms import IngredientAddForm, MenuAddForm, PurchaseForm, RecipeAddForm, 
 from django.urls import reverse
 # bonus
 import numpy as np
+import pandas as pd
 from datetime import datetime
 
 from django.contrib.auth import authenticate, login, logout
@@ -56,6 +57,7 @@ class TimedStringsList(LoginRequiredMixin, ListView):
 
 # temp - trying to avoid creating a new...something
 # very, very likely not the ideal way to do this  - check both projects for ideas
+# complete - it works, but see the class-based implementation below
 @login_required
 def temp_purchase_list(request):
     context = {}
@@ -103,6 +105,10 @@ class ProfitAndLoss(LoginRequiredMixin, TemplateView):
     def total_cost_and_rev(self):
         # as above
         cost, revenue = 0.0, 0.0
+        # new - we're making a dataframe to break things down by date
+        # we want a dataframe w/ an id, rev, cost, profit, and the date
+        pandl_by_date_df = pd.DataFrame(columns=['purchase_id', 'cost', 'revenue', 
+                                                 'profit', 'date'])
         all_purchases = Purchase.objects.all()
 
         for purchase in all_purchases:
@@ -116,17 +122,26 @@ class ProfitAndLoss(LoginRequiredMixin, TemplateView):
             purchase_cost = sum([item.ingredient.price_per_unit * item.ingredient_quantity * purchase_quantity 
                                 for item in purchase_recipe])
             cost += purchase_cost
+            # and now we toss the relevant data into our dataframe
+            purchase_id = purchase.pk  # might work?
+            # there's a way more elegant solution here with two lists and zip, but for legibility:
+            purchase_df_row = {'purchase_id': purchase_id, 'cost': purchase_cost,
+                               'revenue': purchase_revenue, 'profit': 0, 'date': purchase.purchase_timestamp}
+            pandl_by_date_df = pandl_by_date_df.append(purchase_df_row, ignore_index=True)
 
         profit = revenue - cost
+        pandl_by_date_df['profit'] = pandl_by_date_df.revenue - pandl_by_date_df.cost  # should work
         # note: I think I'd have to add in a groupby(date) myself
-        return cost, revenue, profit
+        profit_by_date_df = pandl_by_date_df.groupby('date').profit.sum().reset_index()
+        return cost, revenue, profit, pandl_by_date_df.to_html(), profit_by_date_df.to_html()  # internet trick - studygyaan
 
     # mostly cloned from elMolo example
     def get_context_data(self):
         # base context
         context = super().get_context_data()
         # custom context
-        context['cost'], context['revenue'], context['profit'] = self.total_cost_and_rev()
+        context['cost'], context['revenue'], context['profit'],\
+            context['pandl_by_date_df'], context['profit_by_date_df'] = self.total_cost_and_rev()
         return context
 
 '''
